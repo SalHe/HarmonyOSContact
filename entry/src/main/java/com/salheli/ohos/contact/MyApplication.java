3b6946284727dbf5ab02a99424532a0ea63e5d1d
SalHe <SalHe@qq.com>
2021-05-21 15:05:38 +0800

    init

---
+++ entry/src/main/java/com/salheli/ohos/contact/MyApplication.java
@@ -0,0 +1,10 @@
+package com.salheli.ohos.contact;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/test/java/com/salheli/ohos/contact/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.salheli.ohos.contact;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
